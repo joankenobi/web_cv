9c8972ac7e3d61935f6ff816ae015dcae3492459
joankenobi <joan1233blanco@gmail.com>
2024-09-01 17:24:19 -0400

    feat: skills in the left

--- src/pages/curriculum.tsx
+++ src/pages/curriculum.tsx
@@ -10,7 +10,7 @@ const dataContact = {
   phone: "+58 412 2129362",
   location: "Venezuela, Caracas",
   linkedin: {
-    text:"linkedin.com/in/joan-blanco",
+    text: "linkedin.com/in/joan-blanco",
     url: "https://www.linkedin.com/in/joan-blanco/",
   },
   github: {
@@ -68,18 +68,19 @@ const skills = {
   titulo: "Skills",
   text: [
     // { name: "JavaScript & TypeScript", level: "Semi senior ◉◉○○○" },
-    { name: "JavaScript & TypeScript", level: "Semi senior ◉◉○○○" },
-    { name:"React",},
-    { name:"Node.js",},
-    { name:"Python",},
-    { name:"SQL & NoSQL databases",},
-    { name:"RESTful APIs",},
-    { name:"Django",},
-    { name:"Docker",},
-    { name:"AWS",},
-    { name:"Git",},
-    { name:"Agile/Scrum",},
-    { name:"CI/CD",},
+    { name: "JavaScript", level: "Semi senior ◉◉○○○" },
+    { name: "TypeScript", },
+    { name: "React", },
+    { name: "Node.js", },
+    { name: "Python", },
+    { name: "SQL & NoSQL databases", },
+    { name: "RESTful APIs", },
+    { name: "Django", },
+    { name: "Docker", },
+    { name: "AWS", },
+    { name: "Git", },
+    { name: "Agile/Scrum", },
+    { name: "CI/CD", },
   ],
 }
 
@@ -99,16 +100,16 @@ export function CurriculumPage() {
       <header className="mb-8">
         {/* ENCABEZADO */}
         <h1 className="text-4xl font-bold mb-2">{encabezado.job}</h1>
-        <h2 className="text-2xl text-gray-600 mb-4">{encabezado.name}</h2>
+        <h2 className="text-2xl text-gray-600 mb-3">{encabezado.name}</h2>
         {/* DATOS DE CONTACTO */}
         <div className="flex flex-wrap gap-4">
           <div className="flex items-center">
             <Mail className="w-5 h-5 mr-2" />
-            <a href={"mailto:"+dataContact.mail} className="hover:underline">{dataContact.mail}</a>
+            <a href={"mailto:" + dataContact.mail} className="hover:underline">{dataContact.mail}</a>
           </div>
           <div className="flex items-center">
             <Phone className="w-5 h-5 mr-2" />
-            <a href={"tel:"+dataContact.phone} className="hover:underline">{dataContact.phone}</a>
+            <a href={"tel:" + dataContact.phone} className="hover:underline">{dataContact.phone}</a>
           </div>
           <div className="flex items-center">
             <MapPin className="w-5 h-5 mr-2" />
@@ -125,69 +126,74 @@ export function CurriculumPage() {
         </div>
       </header>
 
+    <div className="flex">
+      <div className='mr-10'>
+        {/* SKILLS */}
+        <section className='mb-2'>
+          <h3 className="text-2xl font-semibold mb-3 border-b border-blue-500 pb-2">{skills.titulo}</h3>
+          <ul className="list-disc columns-1">
+            {skills.text.map((item, index) => (
+              <li key={index} className='text-2sm' >
+                {item.name}
+                <p className='text-xs text-center text-gray-500'>
+                  {item.level}
+                </p>
+              </li>
+            ))}
+          </ul>
+        </section>
+
+        {/* PORFOLIO */}
+        <section>
+          <div className='flex items-center justify-center border-b border-blue-500 mb-3 pb-2'>
+            <ArchiveIcon className="w-5 h-5 mr-2" />
+            <h3 className="text-2xl font-semibold">{portfolio.titulo}</h3>
+          </div>
+          <ul className="list-disc columns-1">
+            {portfolio.text.map((item, index) => (
+              <li key={index} className='text-sm'>
+                <a href={item.url} target="_blank" rel="noopener noreferrer" className="hover:underline">{item.name}</a>
+              </li>))
+            }
+          </ul>
+        </section>
+      </div>
+      <div className="">
         {/* RESUMEN */}
-      <section className="mb-8">
-        <h3 className="text-2xl font-semibold mb-4 border-b pb-2">{summary.titulo}</h3>
-        <p>{summary.text}</p>
-      </section>
+        <section className="mb-2">
+          <h3 className="text-2xl font-semibold mb-3 border-b pb-2">{summary.titulo}</h3>
+          <p>{summary.text}</p>
+        </section>
 
         {/* EXPERIENCIA DE TRABAJO */}
-      <section className="mb-8">
-        <h3 className="text-2xl font-semibold mb-4 border-b pb-2">{workExperience.titulo}</h3>
-        {workExperience.text.map((item, index) => (
-          <div key={index} className="mb-6">
-            <h4 className="text-xl font-medium">{item.job}</h4>
-            <p className="text-gray-600">{item.duration}</p>
-            <ul className="list-disc list-inside mt-2">
-              {item.description.map((desc, i) => (
-                <li key={i}>{desc}</li>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </section>
+        <section className="mb-2">
+          <h3 className="text-2xl font-semibold mb-3 border-b pb-2">{workExperience.titulo}</h3>
+          {workExperience.text.map((item, index) => (
+            <div key={index} className="mb-6">
+              <h4 className="text-xl font-medium">{item.job}</h4>
+              <p className="text-gray-600">{item.duration}</p>
+              <ul className="list-disc list-inside mt-2">
+                {item.description.map((desc, i) => (
+                  <li key={i}>{desc}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </section>
 
         {/* EDUCACIÓN */}
-      <section className="mb-8">
-        <h3 className="text-2xl font-semibold mb-4 border-b pb-2">{education.titulo}</h3>
-        {education.text.map((item, index) => (
-          <div key={index}>
-            <h4 className='text-xl font-medium'> {item.title}</h4>
-            <p className='text-gray-600'> {item.institution}</p>
-            <p className='text-gray-600 text-xs'> {item.duration}</p>
-          </div>
-        ))}
-      </section>
-
-        {/* SKILLS */}
-      <section className='mb-8'>
-        <h3 className="text-2xl font-semibold mb-4 border-b border-blue-500 pb-2">{skills.titulo}</h3>
-        <ul className="list-disc list-inside columns-2 sm:columns-3">
-          {skills.text.map((item, index) => (
-            <li key={index} >
-              {item.name}
-              <p className='text-xs text-center text-gray-500'>
-                {item.level}
-              </p>
-            </li>
+        <section className="mb-2">
+          <h3 className="text-2xl font-semibold mb-3 border-b pb-2">{education.titulo}</h3>
+          {education.text.map((item, index) => (
+            <div key={index}>
+              <h4 className='text-xl font-medium'> {item.title}</h4>
+              <p className='text-gray-600'> {item.institution}</p>
+              <p className='text-gray-600 text-xs'> {item.duration}</p>
+            </div>
           ))}
-        </ul>
-      </section>
-      
-          {/* PORFOLIO */}
-      <section>
-        <div className='flex items-center justify-center border-b border-blue-500 mb-4 pb-2'>
-          <ArchiveIcon className="w-5 h-5 mr-2"/>
-          <h3 className="text-2xl font-semibold">{portfolio.titulo}</h3>
-        </div>
-        <ul className="list-disc list-inside columns-2 sm:columns-3">
-          {portfolio.text.map((item, index) => (
-            <li key={index}>
-            <a href={item.url} target="_blank" rel="noopener noreferrer" className="hover:underline">{item.name}</a>
-            </li>))
-          }
-        </ul>
-      </section>
+        </section>
+      </div>
+      </div>
     </div>
   )
 }
